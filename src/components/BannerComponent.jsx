af17c879299424ff6243457253ae64ce4e83b35b
alfiy <alfi.cakap@gmail.com>
2020-12-17 13:11:20 +0700

    make lazy load background

--- src/components/BannerComponent.jsx
+++ src/components/BannerComponent.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Hero } from "../styled-components/banner";
 
 export const BannerComponent = ({ heroImage, children }) => {
+  const [imageUrl, setImageUrl] = useState("");
+  useEffect(() => {
+    const imageLoader = new Image();
+    imageLoader.src = heroImage;
+
+    imageLoader.onload = () => setImageUrl(heroImage);
+  }, [heroImage]);
   return (
-    <Hero imgUrl={heroImage} fluid>
+    <Hero imgUrl={imageUrl} fluid>
       {children}
     </Hero>
   );
